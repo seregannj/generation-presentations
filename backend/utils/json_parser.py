f8ba9023366cd6ee51ed118c101264a413903ead
seregannj <seregannj@gmail.com>
2026-04-07 15:21:00 +0300

    Create json_parser.py

---
+++ backend/utils/json_parser.py
@@ -0,0 +1,9 @@
+import json
+
+def safe_parse_json(text: str):
+    try:
+        return json.loads(text)
+    except:
+        start = text.find("{")
+        end = text.rfind("}") + 1
+        return json.loads(text[start:end])
