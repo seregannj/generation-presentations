684028731ad6e1a05bad03477fe6db6fce736829
seregannj <seregannj@gmail.com>
2026-04-07 19:46:14 +0300

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@
 import os
 
 # G4F API конфигурация
-G4F_API_KEY = os.getenv("G4F_API_KEY", "g4f_u_mnlj3n_97afcf132daff7aafe278ee6e26c58b39cc4920465080170_794b53f6")
+G4F_API_KEY = os.getenv("G4F_API_KEY", "key")
 G4F_BASE_URL = os.getenv("G4F_BASE_URL", "https://g4f.space/v1")
 
 # CORS (для разных доменов)
