9aea2586e1d9ec81bc599ae44647272cb95ade9c
seregannj <seregannj@gmail.com>
2026-04-09 10:24:46 +0300

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,12 +1,8 @@
 import os
 
-# G4F configuration
-G4F_API_KEY = os.getenv("G4F_API_KEY", "key")
-G4F_BASE_URL = os.getenv("G4F_BASE_URL", "url")
-
-# CORS origins
 ALLOWED_ORIGINS = [
+    "https://seregannj.github.io",
     "https://seregannj.github.io/gendoc",
     "https://seregannj.github.io/gendoc/console",
-    "https://seregannj.github.io/gendoc/console/*"
+    "https://seregannj.github.io/gendoc/console/doclad"
 ]
